e4dcf5b967957e77f972a1234f355efc0652f254
Josh Waihi <josh.waihi@acquia.com>
2017-11-07 08:58:11 +1300

    Added PolicyChain to manage policy dependency

---
+++ src/PolicyChain.php
@@ -0,0 +1,68 @@
+<?php
+namespace Drutiny;
+
+/**
+ * Provides an ordered list of Policies and dependencies.
+ */
+class PolicyChain {
+  protected $chain = [];
+
+  protected $policies;
+
+  /**
+   * Determine if the chain has been built.
+   */
+  protected $built = FALSE;
+
+  public function __construct()
+  {
+    $this->policies = Registry::policies();
+  }
+
+  /**
+   * Add a policy to the policy chain.
+   */
+  public function add(Policy $policy)
+  {
+    // Don't add policy if it has already been added.
+    if (isset($this->chain[$policy->get('name')])) {
+      return TRUE;
+    }
+
+    // Since we're adding to the chain, the chain will need to be rebuilt.
+    $this->built = FALSE;
+    $this->chain[$policy->get('name')] = $policy;
+
+    if ($depends = $policy->get('depends')) {
+      foreach ($depends as $name) {
+        if (!isset($this->policies[$name])) {
+          new \InvalidArgumentException($policy->get('name') . " depends on the '$name' policy but is not available.");
+        }
+        $this->add($this->policies[$name]);
+      }
+    }
+    return TRUE;
+  }
+
+  public function getPolicies()
+  {
+    if ($this->built) {
+      return $this->chain;
+    }
+
+    usort($this->chain, function ($a, $b) {
+      if (in_array($a->get('name'), $b->get('depends'))) {
+        return -1;
+      }
+      if (in_array($b->get('name'), $a->get('depends'))) {
+        return 1;
+      }
+      return 0;
+    });
+
+    $this->built = TRUE;
+    return $this->chain;
+  }
+}
+
+ ?>
