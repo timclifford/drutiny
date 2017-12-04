b6899fcd24545300b1219650e6f471b1efdcf4a5
Josh Waihi <josh.waihi@acquia.com>
2017-12-04 17:12:32 +1300

    Added default audits for testing

---
+++ src/Audit/AlwaysNA.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drutiny\Audit;
+
+use Drutiny\Audit;
+use Drutiny\Sandbox\Sandbox;
+
+/**
+ * An audit that always succeeds.
+ *
+ * This can be used by policies that utilise dependencies as their means of
+ * auditing. As dependencies run first, this audit won't pass until all of
+ * its dependencies pass first.
+ */
+class AlwaysNA extends Audit {
+
+  public function audit(Sandbox $sandbox)
+  {
+    return Audit::NOT_APPLICABLE;
+  }
+}
+
+ ?>
