cd1969ac80cfbc769ada0b0536a8b5a269e70904
Josh Waihi <josh.waihi@acquia.com>
2017-12-05 21:45:03 +1300

    Use Audit require validation functions to confirm N/A audit

--- src/Audit/AlwaysNA.php
+++ src/Audit/AlwaysNA.php
@@ -6,17 +6,19 @@ use Drutiny\Audit;
 use Drutiny\Sandbox\Sandbox;
 
 /**
- * An audit that always succeeds.
- *
- * This can be used by policies that utilise dependencies as their means of
- * auditing. As dependencies run first, this audit won't pass until all of
- * its dependencies pass first.
+ * An audit that always not applicable.
  */
 class AlwaysNA extends Audit {
 
+  protected function requireIneligibility(Sandbox $sandbox)
+  {
+    throw new \Exception("This target is not applicable to " . __CLASS__);
+  }
+
   public function audit(Sandbox $sandbox)
   {
-    return Audit::NOT_APPLICABLE;
+    // This should never trigger.
+    return Audit::FAIL;
   }
 }
 
