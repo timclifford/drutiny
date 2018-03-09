e307a1dba8dcb45a57f95736c5326d0b106203d4
Josh Waihi <josh.waihi@acquia.com>
2018-03-09 16:05:24 +1300

    Improved caching strategy for ini get audit

--- src/Audit/Drupal/IniGet.php
+++ src/Audit/Drupal/IniGet.php
@@ -4,9 +4,25 @@ namespace Drutiny\Audit\Drupal;
 
 use Drutiny\Audit\AbstractComparison;
 use Drutiny\Sandbox\Sandbox;
+use Drutiny\Annotation\Param;
 
 /**
  * Evaluate a PHP ini setting.
+ * @Param(
+ *  name = "setting"
+ *  description = "The name of the ini setting to check."
+ *  type = "string"
+ * )
+ * @Param(
+ *  name = "value"
+ *  description = "The local value of the ini setting to compare for."
+ *  type = "mixed"
+ * )
+ * @Param(
+ *  name = "comp_type"
+ *  description = "The comparison operator to use for the comparison."
+ *  type = "string"
+ * )
  */
 class IniGet extends AbstractComparison {
 
@@ -15,14 +31,16 @@ class IniGet extends AbstractComparison {
    */
   public function audit(Sandbox $sandbox)
   {
+    $ini = $this->sandbox->drush()->evaluate(function () {
+      return ini_get_all();
+    });
     $setting = $sandbox->getParameter('setting');
-    $command = strtr("echo json_encode(ini_get('%setting'));", [
-      '%setting' => $setting
-    ]);
-    $command = "\"$command\"";
-    $value = $sandbox->drush(['format' => 'json'])->ev($command);
 
-    return $this->compare($sandbox->getParameter('value'), $value, $sandbox);
+    if (!isset($ini[$setting])) {
+      return FALSE;
+    }
+
+    return $this->compare($sandbox->getParameter('value'), $ini[$setting]['local_value'], $sandbox);
   }
 
 }
