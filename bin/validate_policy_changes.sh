3570f7074b85059fc9dca3082bd8472be19cd8c8
Josh Waihi <josh.waihi@acquia.com>
2018-03-22 16:14:54 +1300

    Allow script to be used by plugins

--- bin/validate_policy_changes.sh
+++ bin/validate_policy_changes.sh
@@ -3,9 +3,15 @@
 MAIN_BRANCH=$1
 MERGING_BRANCH=$2
 
+DRUTINY="./bin/drutiny"
+
+if [ -f "./vendor/bin/drutiny" ]; then
+	DRUTINY="./vendor/bin/drutiny"
+fi
+
 POLICIES=`git log --stat --oneline $MAIN_BRANCH..$MERGING_BRANCH | grep ' | ' | grep 'policy.yml' | awk '{print $1}'`
 
 for policy in $POLICIES; do
 	name=`cat $policy | egrep '^name: ' | awk '{print $2}' | head -1`
-	./bin/drutiny policy:info $name -vvv
+	$DRUTINY policy:info $name -vvv
 done
