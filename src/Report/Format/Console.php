3aeed1f9c23f0f3d1ee9ce5e80e8e5725d7031bc
Josh Waihi <josh.waihi@acquia.com>
2018-03-27 12:39:42 +1300

    Removed need for ProfileRunReport in Console Format

--- src/Report/Format/Console.php
+++ src/Report/Format/Console.php
@@ -2,11 +2,14 @@
 
 namespace Drutiny\Report\Format;
 
-use Drutiny\Report\ProfileRunReport;
 use Drutiny\Report\Format;
+use Drutiny\AuditResponse\AuditResponse;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Helper\TableSeparator;
+
 
 class Console extends Format {
 
@@ -53,8 +56,63 @@ class Console extends Format {
 
   public function render($profile, $target, $result)
   {
-    $report = new ProfileRunReport($profile, $target, $result);
-    $report->render($this->input, $this->output);
+    $io = new SymfonyStyle($this->input, $this->output);
+    $io->title($profile->getTitle());
+
+    $table_rows = [];
+    $pass = [];
+    foreach ($result as $response) {
+      $pass[] = $response->isSuccessful();
+
+      $summary = [];
+      foreach (explode(PHP_EOL, $response->getSummary()) as $line) {
+        if (strlen($line) < 100) {
+          $summary[] = $line;
+          continue;
+        }
+        $words = explode(' ', $line);
+        $phrase = [];
+        while ($word = array_shift($words)) {
+          $compound_line = $phrase;
+          $compound_line[] = $word;
+          if (strlen(implode(' ', $compound_line)) > 100) {
+            $summary[] = implode(' ', $phrase);
+            $phrase = ["    "];
+          }
+          $phrase[] = $word;
+        }
+        $summary[] = implode(' ', $phrase);
+      }
+      $summary = implode(PHP_EOL, $summary);
+
+      $table_rows[] = [
+        $this->getIcon($response),
+        $response->getTitle(),
+        $response->getSeverity(),
+        $summary . (
+          $response->isSuccessful() ? '' : PHP_EOL . PHP_EOL . $response->getRemediation()
+        ),
+      ];
+      $table_rows[] = new TableSeparator();
+    }
+
+    $total_tests = count($result);
+    $total_pass = count(array_filter($pass));
+    $table_rows[] = ['', "$total_pass/$total_tests passed", ''];
+    $io->table(['', 'Policy', 'Severity', 'Summary'], $table_rows);
+  }
+
+  protected function getIcon(AuditResponse $response)
+  {
+    if ($response->isNotice()) {
+      return "ℹ️";
+    }
+    elseif ($response->hasWarning()) {
+      return "⚠️";
+    }
+    else {
+      return $response->isSuccessful() ? "✅" : "❌";
+    }
   }
 }
 
