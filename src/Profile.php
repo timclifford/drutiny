a2ac1317fdc2bd1adc52529574a1af9e4dc38248
Josh Waihi <josh.waihi@acquia.com>
2018-04-02 10:08:32 +1200

    Added check for v2.0 profile schema

--- src/Profile.php
+++ src/Profile.php
@@ -87,7 +87,12 @@ class Profile {
     }
 
     if (isset($info['policies'])) {
+      $v21_keys = ['parameters', 'severity'];
       foreach ($info['policies'] as $name => $metadata) {
+        // Check for v2.0.x style profiles.
+        if (!empty($metadata) && !count(array_intersect($v21_keys, array_keys($metadata)))) {
+          throw new \Exception("{$info['title']} is a v2.0.x profile. Please upgrade $filepath to v2.1.x schema.");
+        }
         $weight = array_search($name, array_keys($info['policies']));
         $profile->addPolicyDefinition(PolicyDefinition::createFromProfile($name, $weight, $metadata));
       }
