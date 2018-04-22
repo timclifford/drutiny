7ac5a915a8a206a8f25f76a17178fab33dc06d5e
Josh Waihi <josh.waihi@acquia.com>
2018-04-23 09:37:37 +1000

    Convert FsSize audit to actually measure filesystem size

--- src/Audit/Filesystem/FsSize.php
+++ src/Audit/Filesystem/FsSize.php
@@ -20,38 +20,19 @@ class FsSize extends Audit {
 
     $path = strtr($path, $stat['%paths']);
 
-    $root = $stat['root'];
-    $files = $stat['files'];
+    $size = $sandbox->exec("du -d 0 $path | awk '{print $1}'");
 
     $max_size = (int) $sandbox->getParameter('max_size', 20);
 
-    $command = "find @location -type f -size +@sizeM -printf '@print-format'";
-    $command .= " | sort -nr";
-    $command = strtr($command, [
-      '@location' => "{$root}/{$files}/",
-      '@size' => $max_size,
-      '@print-format' => '%k\t%p\n',
-    ]);
+    // Set the size in MB for rendering
+    $sandbox->setParameter('size', ceil($size * 1024 * 1024));
+    // Set the actual path.
+    $sandbox->setParameter('path', $path);
 
-    $output = $sandbox->exec($command);
+    // Convert max_size into bytes.
+    $max_size = $max_size * 1024 * 1024;
 
-    if (empty($output)) {
-      return TRUE;
-    }
-
-    // Output from find is a giant string with newlines to seperate the files.
-    $rows = array_map(function ($line) {
-      $parts = array_map('trim', explode("\t", $line));
-      $size = number_format((float) $parts[0] / 1024, 2);
-      $filename = trim($parts[1]);
-      return "{$filename} [{$size} MB]";
-    },
-    array_filter(explode("\n", $output)));
-
-    $sandbox->setParameter('issues', $rows);
-    $sandbox->setParameter('plural', count($rows) > 1 ? 's' : '');
-
-    return Audit::WARNING;
+    return $size < $max_size;
   }
 
 }
