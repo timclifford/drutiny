c0573bbd1d22544452d1d8bd08554861286bbe22
Brian Schilt <brian.schilt@acquia.com>
2018-04-25 21:53:19 -0700

    fixed php warning and adjusted max_size to 10 mb.

--- src/Audit/Drupal/LargeDrupalFiles.php
+++ src/Audit/Drupal/LargeDrupalFiles.php
@@ -44,17 +44,17 @@ class LargeDrupalFiles extends Audit {
    * @inheritdoc
    */
   public function audit(Sandbox $sandbox) {
-    $max_size = (int) $sandbox->getParameter('max_size', 20000000);
+    $max_size = (int) $sandbox->getParameter('max_size', 10000000);
     $sandbox->setParameter('readable_max_size', $max_size / 1000 / 1000 . ' MB');
     $query = "SELECT fm.uri, fm.filesize, (SELECT COUNT(*) FROM file_usage fu WHERE fu.fid = fm.fid) as 'usage' FROM file_managed fm WHERE fm.filesize >= @size ORDER BY fm.filesize DESC";
     $query = strtr($query, ['@size' => $max_size]);
     $output = $sandbox->drush()->sqlQuery($query);
-
+print_r($output);
     if (empty($output)) {
       return TRUE;
     }
 
-    $records = explode("\n", $output);
+    $records = is_array($output) ? $output : explode("\n", $output);
     $rows = array();
     foreach ($records as $record) {
       // Ignore record if it contains message about adding RSA key to known hosts.
