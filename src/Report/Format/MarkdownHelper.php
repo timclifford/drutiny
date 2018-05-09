70eb1966bf373c6405fae80ee045763950e1a84d
Josh Waihi <josh.waihi@acquia.com>
2018-05-09 15:39:48 +1200

    Fixed a parse bug in Parsedown library

--- src/Report/Format/MarkdownHelper.php
+++ src/Report/Format/MarkdownHelper.php
@@ -7,6 +7,13 @@ use Parsedown;
 class MarkdownHelper extends Parsedown {
   // Remove Code as a block type. This prevents code blocks occuring
   // from indentation as it becomes confusing in YAML template files.
-  protected $unmarkedBlockTypes = array();
+  protected $unmarkedBlockTypes = array('Foo');
+
+  // $unmarkedBlockTypes cannot be empty due to a parsing bug so blockFoo() must
+  // be defined.
+  protected function blockFoo()
+  {
+    return;
+  }
 }
  ?>
