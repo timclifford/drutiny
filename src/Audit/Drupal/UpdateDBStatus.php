7143a3503f9122602fda64e098d285eb06a2bdf0
Josh Waihi <josh.waihi@acquia.com>
2019-07-11 16:42:06 +1200

    Fix Database updates bug.

--- src/Audit/Drupal/UpdateDBStatus.php
+++ src/Audit/Drupal/UpdateDBStatus.php
@@ -11,15 +11,33 @@ use Drutiny\Sandbox\Sandbox;
 class UpdateDBStatus extends Audit {
 
   /**
+   * A string which presence indicates updates are pending.
+   */
+  const AFFIRMATIVE_UPDATES_STRING = "Do you wish to run all pending updates";
+
+  /**
    * @inheritdoc
    */
   public function audit(Sandbox $sandbox) {
     $output = $sandbox->drush()->updb('-n');
 
-    if (strpos($output, 'No database updates required') >= 0 || empty($output)) {
-      return TRUE;
-    }
-    return FALSE;
-  }
+    if (strpos($output, self::AFFIRMATIVE_UPDATES_STRING) !== FALSE) {
 
+      $lines = array_filter(explode(PHP_EOL, $output));
+      $updates = [];
+      while (strpos(current($lines), self::AFFIRMATIVE_UPDATES_STRING) === FALSE) {
+        preg_match("/\s*([\w\s]*\w)\s+(\d+)\s+(.*)/", current($lines), $matches);
+        list(, $module, $revision, $message) = $matches;
+        $updates[] = [
+          'module' => $module,
+          'revision' => $revision,
+          'message' => $message
+        ];
+        next($lines);
+      }
+      $sandbox->setParameter('updates', $updates);
+      return FALSE;
+    }
+    return TRUE;
+  }
 }
