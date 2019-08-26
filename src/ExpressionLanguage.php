3e9e6c97a78e3d794ecce1a453180a78e982395e
Josh Waihi <josh.waihi@acquia.com>
2019-08-27 08:33:13 +1200

    Added more php functions to expression language.

--- src/ExpressionLanguage.php
+++ src/ExpressionLanguage.php
@@ -19,5 +19,33 @@ class ExpressionLanguage extends BaseExpressionLanguage
 
         $this->addFunction(ExpressionFunction::fromPhp('in_array'));
         $this->addFunction(ExpressionFunction::fromPhp('array_key_exists'));
+        $this->addFunction(ExpressionFunction::fromPhp('count'));
+        $this->addFunction(ExpressionFunction::fromPhp('array_filter'));
+        $this->addFunction(new ExpressionFunction('filter',
+          // Compiler
+          function ($array, $property, $match) {
+            return sprintf('filter(%s, "%s", "%s")', $array, $property, $match);
+          },
+          // Evaluator
+          function ($array, $property, $match = TRUE) {
+            return array_filter($array, function ($value) use ($property, $match) {
+              if (!is_array($value)) {
+                return FALSE;
+              }
+              $property_parts = explode('.', $property);
+              $ref = $value;
+              foreach ($property_parts as $key) {
+                if (!isset($ref[$key])) {
+                  return FALSE;
+                }
+                $ref = $ref[$key];
+              }
+              if ($ref == $match) {
+                return TRUE;
+              }
+              return FALSE;
+            });
+          }
+        ));
     }
 }
