d3dc0d89dbe097842253e286979bbcaa59466dac
Josh Waihi <josh.waihi@acquia.com>
2020-03-27 19:07:46 +1300

    Added new expression functions to make language easier to evaluate API data.

--- src/ExpressionLanguage.php
+++ src/ExpressionLanguage.php
@@ -21,14 +21,53 @@ class ExpressionLanguage extends BaseExpressionLanguage
         $this->addFunction(ExpressionFunction::fromPhp('array_key_exists'));
         $this->addFunction(ExpressionFunction::fromPhp('count'));
         $this->addFunction(ExpressionFunction::fromPhp('array_filter'));
+
+        // Return an array from an array based on the value of one of its keyed properties.
+        $this->addFunction(new ExpressionFunction('array_select',
+          // Compiler
+          function () {
+            list($input, $property, $value, $strict_match) = array_slice(func_get_args(), 1);
+            return sprintf('array_select(<input_array>, "%s", "%s", "%b")', $property, $value, $strict_match);
+          },
+
+          // Evaluator
+          function () {
+            list($input, $property, $value, $strict_match) = array_slice(func_get_args(), 1);
+            foreach ($input as $set) {
+              if (!isset($set[$property])) {
+                continue;
+              }
+              if ($strict_match && ($set[$property] == $value)) {
+                return $set;
+              }
+              if (!$strict_match && (strpos($set[$property], $value) !== FALSE)) {
+                return $set;
+              }
+            }
+            return [
+              $property => FALSE,
+            ];
+          })
+        );
+
+        /**
+         * Filter an array of arrays by property.
+         *
+         * @param $array Array The input array.
+         * @param $property String the property to compare in each array.
+         * @param $match Mixed the value to compare with each property value.
+         * @param $equals Bool Whether to do an equal comparison or "contains" comparison.
+         */
         $this->addFunction(new ExpressionFunction('filter',
           // Compiler
-          function ($array, $property, $match) {
-            return sprintf('filter(%s, "%s", "%s")', $array, $property, $match);
+          function ($array, $property, $match, $equals) {
+            list($array, $property, $match, $equals) = array_slice(func_get_args(), 1);
+            return sprintf('filter(%s, "%s", "%s", "%s")', $array, $property, $match, $equals);
           },
           // Evaluator
-          function ($array, $property, $match = TRUE) {
-            return array_filter($array, function ($value) use ($property, $match) {
+          function ($array, $property, $match = TRUE, $equals = TRUE) {
+            list($array, $property, $match, $equals) = array_slice(func_get_args(), 1);
+            return array_filter($array, function ($value) use ($property, $match, $equals) {
               if (!is_array($value)) {
                 return FALSE;
               }
@@ -40,10 +79,8 @@ class ExpressionLanguage extends BaseExpressionLanguage
                 }
                 $ref = $ref[$key];
               }
-              if ($ref == $match) {
-                return TRUE;
-              }
-              return FALSE;
+
+              return $equals ? $ref == $match : strpos($ref, $match) !== FALSE;
             });
           }
         ));
